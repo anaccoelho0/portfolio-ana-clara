18ffdc14b8d3c582de5700cadeae715add574eeb
anaccoelho0 <anaclaralcoelho2006@gmail.com>
2025-09-24 11:32:46 -0300

    Projeto

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import NavBar from './components/NavBar.vue'
+import Carrossel from './components/Carrossel.vue'
+
+</script>
+
+<template>
+
+  <router-view />
+  <NavBar />
+  <Carrossel />
+
+</template>
+
+
+
+<style scoped>
+
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+<nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand">Ana Clara Coelho</a>
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar'
+}
+
+</script>
+
+<style scoped>
+
+</style>
